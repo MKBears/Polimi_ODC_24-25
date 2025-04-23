1b86a54884294398ddca9e0726f99123e1a06936
MKBears <marco2.orsi@mail.polimi.it>
2025-04-23 19:19:40 +0200

    Try to start forking server exploit

--- shellcoding/fs/x.py
+++ shellcoding/fs/x.py
@@ -1,19 +1,27 @@
 from pwn import *
 
 # the * before the address means to gdb "address" (duh) instead of "symbol" (like the main function)
-COMMANDS = """
-b  main
+COMMANDS = '''
+brva 0x40146D
 c
-"""
-context.arch = "amd64"
+'''
+context.arch = 'amd64'
+exe = './forking_server'
+target = '0.0.0.0'
+port = 4000
 
 if args.REMOTE:
-    c = remote(".training.offensivedefensive.it", 8080, ssl=True)
+    c = remote('forking-server.training.offensivedefensive.it', 8080, ssl=True)
 else:
     if args.GDB:
-        c = gdb.debug("", gdbscript = COMMANDS)
+        # for some reason, gdb.debug(exe, gdbscripts = COMMANDS) could not find the executable...
+        # serv = process(exe)
+        # serv = gdb.attach(serv, gdbscript = COMMANDS)
+        serv = gdb.debug(exe, gdbscript=COMMANDS)
     else:
-        c = process("")
+        serv = process(exe)
 
-c.recvuntil(b"")
+    c = remote(target, port)
+
+c.recvuntil(b'name?\n')
 c.interactive()
